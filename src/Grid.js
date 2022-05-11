e0d72fe84fde808701f396b899d21fa6b3d694a7
Ori Nizri Tal <ori.nizri.tal@gmail.com>
2022-05-11 15:16:36 +0300

    first commit

---
+++ src/Grid.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Grid = ({ config, data }) => (
+  <table>
+    <thead>
+      <tr>
+        {config.map(column => {
+          return (
+            <th>
+              {column.title}
+            </th>
+          )
+        })}
+      </tr>
+    </thead>
+    <tbody>
+      {data.map(row => {
+        return (
+          <>
+            <tr>
+              {
+                config.map(column => {
+                  if (row[column.field].hasOwnProperty('url')) {
+                    return (
+                      (
+                        <td key={row[column.field].url}>{row[column.field].url}</td>
+                      )
+                    )
+                  }
+                  return (
+                    <td key={row[column.field]}>
+                      {row[column.field]}
+                    </td>
+                  )
+                })
+              }
+            </tr>
+          </>
+        )
+      })}
+
+    </tbody>
+  </table>
+);
+
+export default Grid;
